bfa2e351a212104ad5be0cf0ba3b9b08c35bbc0b
Martin Vercammen <martin2.vercammen2@gmail.com>
2023-05-05 23:45:16 +0200

    Add first player movement

--- KISS_FART/Assets/CODE/Scripts/DinoMovement.cs
+++ KISS_FART/Assets/CODE/Scripts/DinoMovement.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class DinoMovement : MonoBehaviour
 {
-   private float test;
+  [SerializeField] private CharacterController m_controller ;
+  [SerializeField]  float m_speed;
+
+    private void Awake()
+    {
+    }
+
+    private void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical) * m_speed * Time.deltaTime;
+        m_controller.Move(move);
+    }
 }
