50575283f23322b8fdb859033b75cabea746f3f7
Martin Vercammen <martin2.vercammen2@gmail.com>
2023-05-07 12:31:51 +0200

    Add egg score to HUD

--- KISS_FART/Assets/CODE/Scripts/HUDManager.cs
+++ KISS_FART/Assets/CODE/Scripts/HUDManager.cs
@@ -9,14 +9,47 @@ public class HUDManager : MonoBehaviour
     public Sprite m_unfriendlyChickenText;
     public Sprite m_takeEggText;
     public Sprite m_takenEggText;
+    public Sprite m_0EggScore;
+    public Sprite m_1EggScore;
+    public Sprite m_2EggScore;
+    public Sprite m_3EggScore;
+    public Sprite m_4EggScore;
+    public Sprite m_5EggScore;
     public Image m_hudTimeText;
     public Image m_hudEggText;
+    public Image m_hudScoreEggText;
 
-   
 
     // Update is called once per frame
     void Update()
     {
+
+        if (GameManager.s_poulerScore == 0)
+        {
+            m_hudEggText.sprite = m_0EggScore;
+        }
+        else if (GameManager.s_poulerScore == 1)
+        {
+            m_hudEggText.sprite = m_1EggScore;
+        }
+        else if(GameManager.s_poulerScore == 2)
+        {
+            m_hudEggText.sprite = m_2EggScore;
+        }
+        else if(GameManager.s_poulerScore == 3)
+        {
+            m_hudEggText.sprite = m_3EggScore;
+        }
+        else if(GameManager.s_poulerScore == 4)
+        {
+            m_hudEggText.sprite = m_4EggScore;
+        }
+        else if(GameManager.s_poulerScore == 5)
+        {
+            m_hudEggText.sprite = m_5EggScore;
+        }
+
+
         if (GameManager.s_instance.m_currentTime == GameManager.ETimeState.Day)
         {
             m_hudTimeText.sprite = m_friendlyChickenText;
